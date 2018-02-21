a470d7f2e5bef9afa12358aee15985a3e3fc2a59
Unknown <alvaro.reina@uma.es>
2018-02-21 08:26:35 +0100

    [ADD] CRUD verbs endpoints

--- common/src/main/kotlin/constants/api/Endpoints.kt
+++ common/src/main/kotlin/constants/api/Endpoints.kt
@@ -6,7 +6,7 @@ object Endpoints{
     const val noteEndpoint = "notes"
 
     /**
-     *  GET: return a list of all notes
+     *  GET: returns a list of all notes
      *      Success:    200 OK
      *      Error:      400 Bad request
      *                  500 Internal error
@@ -16,11 +16,39 @@ object Endpoints{
     /**
      *  POST: create a new note, returns note id
      *      Success:    201 Created
-     *      Error:      409 Conflict
+     *      Error:      400 Bad request
+     *                  409 Conflict
      *                  500 Internal error
      *
      */
     const val createNote = "add"
 
+    /**
+     *  getNote + {id}
+     *  GET: returns a note
+     *      Success:    200 OK
+     *      Error:      400 Bad request
+     *                  404 Not found
+     *                  500 Internal error
+     */
+    const val getNote = "get/"
 
+    /**
+     *  updateNote + {id}
+     *  PUT: updates a note
+     *      Success:    204 No content
+     *      Error:      400 Bad request
+     *                  404 Not found
+     *                  500 Internal error
+     */
+    const val updateNote = "add/"
+
+    /**
+     *  removeNote + {id}
+     *  DELETE: removes a note
+     *      Success:    200 OK
+     *      Error:      400 Bad request
+     *                  404 Not found
+     *                  500 Internal error
+     */
 }
